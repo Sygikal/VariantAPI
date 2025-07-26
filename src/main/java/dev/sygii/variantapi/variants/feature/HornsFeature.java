f07deb5ba6ed75b8c0fe0e38014f32d46619da32
Sygikal <xtin8873@gmail.com>
2025-07-26 11:25:04 -0700

    Add color to horns

--- src/main/java/dev/sygii/variantapi/variants/feature/HornsFeature.java
+++ src/main/java/dev/sygii/variantapi/variants/feature/HornsFeature.java
@@ -8,22 +8,32 @@ import net.minecraft.util.Identifier;
 public class HornsFeature extends VariantFeature {
     public static Identifier ID = VariantAPI.id("horns_feature");
     private final Identifier texture;
+    private final int color;
 
-    public HornsFeature(Identifier texture) {
+    public HornsFeature(Identifier texture, int color) {
         super(ID);
         this.texture = texture;
+        this.color = color;
     }
 
     @Override
     public void serialize(PacketByteBuf buf) {
-        buf.writeIdentifier(texture);
+        buf.writeIdentifier(this.texture);
+        buf.writeInt(this.color);
     }
 
     public static HornsFeature deserialize(PacketByteBuf buf) {
-        return new HornsFeature(buf.readIdentifier());
+        return new HornsFeature(buf.readIdentifier(), buf.readInt());
     }
 
     public Identifier getTexture() {
         return this.texture;
+    }
+
+    public float[] getColors() {
+        int j = (color & 0xFF0000) >> 16;
+        int k = (color & 0xFF00) >> 8;
+        int l = (color & 0xFF) >> 0;
+        return new float[]{j / 255.0F, k / 255.0F, l / 255.0F};
     }
 }
