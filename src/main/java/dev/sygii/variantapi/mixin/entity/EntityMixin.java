3c2af90966a06270db16751d27df034409a1eea3
Sygikal <xtin8873@gmail.com>
2025-07-25 17:58:02 -0700

    handle breeding

--- src/main/java/dev/sygii/variantapi/mixin/entity/EntityMixin.java
+++ src/main/java/dev/sygii/variantapi/mixin/entity/EntityMixin.java
@@ -4,8 +4,10 @@ import dev.sygii.variantapi.VariantAPI;
 import dev.sygii.variantapi.acess.EntityAccess;
 import dev.sygii.variantapi.variants.Variant;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.PassiveEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import oshi.util.tuples.Pair;
 
 import java.util.ArrayList;
 
@@ -15,6 +17,10 @@ public abstract class EntityMixin implements EntityAccess {
 	private Variant variant = VariantAPI.getDefaultVariant();
 	@Unique
 	private ArrayList<Variant> variantOverlays = new ArrayList<>();
+	@Unique
+	private Pair<PassiveEntity, PassiveEntity> parents;
+	@Unique
+	private boolean breedingHandled;
 
 	@Override
 	public Variant getVariant() {
@@ -39,5 +45,25 @@ public abstract class EntityMixin implements EntityAccess {
 	@Override
 	public void setVariantOverlays(ArrayList<Variant> variants) {
 		this.variantOverlays = variants;
+	}
+
+	@Override
+	public Pair<PassiveEntity, PassiveEntity> variantapi$getParents() {
+		return this.parents;
+	}
+
+	@Override
+	public void variantapi$setParents(PassiveEntity father, PassiveEntity mother) {
+		this.parents = new Pair<>(father, mother);
+	}
+
+	@Override
+	public boolean isBreedingHandled() {
+		return this.breedingHandled;
+	}
+
+	@Override
+	public void setBreedingHandled(boolean handled) {
+		this.breedingHandled = handled;
 	}
 }
