55c92846ca7a7fa3763158dd0e2c99fa5595ae04
Sygikal <xtin8873@gmail.com>
2025-07-31 22:45:05 -0700

    Custom sounds and display name

--- src/main/java/dev/sygii/variantapi/mixin/entity/EntityMixin.java
+++ src/main/java/dev/sygii/variantapi/mixin/entity/EntityMixin.java
@@ -1,12 +1,25 @@
 package dev.sygii.variantapi.mixin.entity;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.sygii.variantapi.VariantAPI;
 import dev.sygii.variantapi.acess.EntityAccess;
+import dev.sygii.variantapi.acess.SoundEventAccess;
 import dev.sygii.variantapi.variants.Variant;
+import dev.sygii.variantapi.variants.feature.CustomSoundsFeature;
+import dev.sygii.variantapi.variants.feature.DisplayNameFeature;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.text.Text;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import oshi.util.tuples.Pair;
 
 import java.util.ArrayList;
@@ -65,5 +78,27 @@ public abstract class EntityMixin implements EntityAccess {
 	@Override
 	public void setBreedingHandled(boolean handled) {
 		this.breedingHandled = handled;
+	}
+
+	@Inject(method = "getDefaultName", at = @At(value = "RETURN"), cancellable = true)
+	protected void getVariantName(CallbackInfoReturnable<Text> cir) {
+		if (variant.getFeatures().containsKey(DisplayNameFeature.ID)) {
+			cir.setReturnValue(((DisplayNameFeature)variant.getFeatures().get(DisplayNameFeature.ID)).getDisplayName());
+		}
+	}
+
+	@WrapOperation(method = "playSound", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;playSound(Lnet/minecraft/entity/player/PlayerEntity;DDDLnet/minecraft/sound/SoundEvent;Lnet/minecraft/sound/SoundCategory;FF)V"))
+	protected void modifySound(World instance, PlayerEntity except, double x, double y, double z, SoundEvent sound, SoundCategory category, float volume, float pitch, Operation<Void> original) {
+		if (variant.getFeatures().containsKey(CustomSoundsFeature.ID)) {
+			if (((CustomSoundsFeature)variant.getFeatures().get(CustomSoundsFeature.ID)).getSoundMap().containsKey(sound.getId())) {
+				CustomSoundsFeature.CustomSound newSound = ((CustomSoundsFeature)variant.getFeatures().get(CustomSoundsFeature.ID)).getSoundMap().get(sound.getId());
+				//SoundEvent newSound = sound;
+				//((SoundEventAccess)newSound).setId(((CustomSoundsFeature)variant.getFeatures().get(CustomSoundsFeature.ID)).getSoundMap().get(sound.getId()));
+				//System.out.println(newSound.pitch() + " " + pitch);
+				original.call(instance, except, x, y, z, newSound.event(), category, newSound.volume() == 5.0f ? volume : newSound.volume(), newSound.pitch() == 5.0f ? pitch : newSound.pitch());
+			}
+		}else {
+			original.call(instance, except, x, y, z, sound, category, volume, pitch);
+		}
 	}
 }
