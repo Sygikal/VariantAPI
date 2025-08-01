5e160e0f710aa737248fa180adf72613171f7a35
Sygikal <xtin8873@gmail.com>
2025-07-31 22:46:05 -0700

    Server-side features

--- src/main/java/dev/sygii/variantapi/variants/VariantFeature.java
+++ src/main/java/dev/sygii/variantapi/variants/VariantFeature.java
@@ -6,13 +6,24 @@ import net.minecraft.util.Identifier;
 
 public class VariantFeature {
     private final Identifier id;
+    private final boolean serverOnly;
 
     public VariantFeature(Identifier id) {
         this.id = id;
+        this.serverOnly = false;
+    }
+
+    public VariantFeature(Identifier id, boolean serverOnly) {
+        this.id = id;
+        this.serverOnly = serverOnly;
     }
 
     public Identifier getIdentifier() {
         return this.id;
+    }
+
+    public boolean isServerOnly() {
+        return this.serverOnly;
     }
 
     public void serialize(PacketByteBuf buf) {}
